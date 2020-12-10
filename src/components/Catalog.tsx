be50cb85b4003cb9161b11cad929992518dbf9b1
gutovesco <guto.vesco@hotmail.com>
2020-12-10 16:39:38 -0300

    interfaces cart and json server

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api'
 
 // import { Container } from './styles';
 
 const Catalog: React.FC = () => {
-    const store = useSelector(state => state);
+    const [catalog, setCatalog] = useState<IProduct[]>([])
+
+    useEffect(() => {
+        api.get('products').then(response => {
+            setCatalog(response.data);
+        })
+    }, [])
+    
     return (
         <div>
-            Catalog
+            <div>Catalog</div>
+            {catalog.map(item => (
+                <article key={item.id}>
+                    <strong>{item.title}</strong> {" - "}
+                    <span>{item.price}</span>
+
+                    <button type="button">Comprar</button>
+                </article>
+            ))}
         </div>
     )
 }
